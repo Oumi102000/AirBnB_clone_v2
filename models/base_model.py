832d57c159b02933008e86354092af1557526c3d
Oumi102000 <o.elmadani10@gmail.com>
2023-09-14 00:45:39 +0000

    update BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,25 @@ class BaseModel:
     creates BaseModel parent class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         __init__ the class constructor, initializes all instance attributes
+
+        Args:
+            *args: unused arguments
+            **kwargs (dict): key/value pairs
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    else:
+                        self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """returns the BaseModel str representation"""
