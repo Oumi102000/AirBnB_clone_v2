df5cb2d6181728a676f11bd1be07683125d8032b
Oumi102000 <o.elmadani10@gmail.com>
2023-09-14 22:22:02 +0000

    Add an entry point program to the command interpreter

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Defines AirBnB console module
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    contains the entry point of the command interpreter
+    """
+    prompt = "(hbnb) "
+    def do_quit(self, line):
+        """quit command to exit the HBNB console"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the HBNB program"""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Overrides the cmd emptyline method, do nothing"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
