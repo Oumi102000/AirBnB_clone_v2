16987cf0cb366a953c28687a5aafa9d1ed413ba9
Oumi102000 <o.elmadani10@gmail.com>
2023-09-16 00:37:15 +0000

    Implement review class

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines Review module
+"""
+from modules.base_model import BaseModel
+
+class Review(BaseModel):
+    """create review class
+    Attributes:
+        place_id (str): place's id
+        user_id (str): user's id
+        text (str): text review
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
